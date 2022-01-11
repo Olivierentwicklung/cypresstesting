31600486f9c2e5627568d200b1c3514e21d35c60
Olivierentwicklung <olivierentwicklung@gmail.com>
2022-01-11 22:13:24 +0100

    yes

---
+++ cypress/pageobjects/pageactions/OpenAccountPageActions.js
@@ -0,0 +1,26 @@
+/// <reference types="cypress" />
+
+import OpenAccountPageElements from '../pageelements/OpenAccountPageElements'
+
+export default class OpenAccountPageActions{
+
+    constructor(){
+        globalThis.openaccountelement = new OpenAccountPageElements()
+    }
+
+    openaccount(customer,currency){
+        openaccountelement.openAccountBtn().click()
+        openaccountelement.customerDropdown().select(customer)
+        openaccountelement.currencyDropdown().select(currency)
+        openaccountelement.processBtn().click()
+    }
+
+    validateAlert(accountalert){
+        cy.on('window:alert',(alertText)=>{
+            expect(alertText).contains(alertText)
+          })
+
+
+    }
+
+}
